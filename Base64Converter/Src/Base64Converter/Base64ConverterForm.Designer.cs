26e9687f3130c03d263d267191b56490a28b27fb
yutaka-art <union.dml@gmail.com>
2023-09-18 14:59:21 +0900

    Base64 エンコード・デコード 変換ツール

---
+++ Base64Converter/Src/Base64Converter/Base64ConverterForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Base64Converter
+{
+    partial class Base64ConverterForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grpBox01 = new System.Windows.Forms.GroupBox();
+            rdoDecode = new System.Windows.Forms.RadioButton();
+            rdoEncode = new System.Windows.Forms.RadioButton();
+            label1 = new System.Windows.Forms.Label();
+            grpBox01.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpBox01
+            // 
+            grpBox01.Controls.Add(rdoDecode);
+            grpBox01.Controls.Add(rdoEncode);
+            grpBox01.Location = new System.Drawing.Point(29, 12);
+            grpBox01.Name = "grpBox01";
+            grpBox01.Size = new System.Drawing.Size(543, 95);
+            grpBox01.TabIndex = 0;
+            grpBox01.TabStop = false;
+            grpBox01.Text = "エンコードデコード";
+            // 
+            // rdoDecode
+            // 
+            rdoDecode.AutoSize = true;
+            rdoDecode.Location = new System.Drawing.Point(172, 47);
+            rdoDecode.Name = "rdoDecode";
+            rdoDecode.Size = new System.Drawing.Size(89, 29);
+            rdoDecode.TabIndex = 1;
+            rdoDecode.Text = "デコード";
+            rdoDecode.UseVisualStyleBackColor = true;
+            // 
+            // rdoEncode
+            // 
+            rdoEncode.AutoSize = true;
+            rdoEncode.Checked = true;
+            rdoEncode.Location = new System.Drawing.Point(34, 47);
+            rdoEncode.Name = "rdoEncode";
+            rdoEncode.Size = new System.Drawing.Size(102, 29);
+            rdoEncode.TabIndex = 0;
+            rdoEncode.TabStop = true;
+            rdoEncode.Text = "エンコード";
+            rdoEncode.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(28, 124);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(489, 25);
+            label1.TabIndex = 1;
+            label1.Text = "エンコード・デコードを指定し、ファイルをドラッグ＆ドロップしてください。";
+            // 
+            // Base64ConverterForm
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(602, 187);
+            Controls.Add(label1);
+            Controls.Add(grpBox01);
+            Name = "Base64ConverterForm";
+            Text = "Base64ConverterForm";
+            Load += Base64ConverterForm_Load;
+            DragDrop += Base64ConverterForm_DragDrop;
+            DragEnter += Base64ConverterForm_DragEnter;
+            grpBox01.ResumeLayout(false);
+            grpBox01.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBox01;
+        private System.Windows.Forms.RadioButton rdoDecode;
+        private System.Windows.Forms.RadioButton rdoEncode;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Base64Converter/Src/Base64Converter/Base64ConverterForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Base64Converter
+{
+    public partial class Base64ConverterForm : Form
+    {
+        /// <summary>
+        /// InitializeComponent
+        /// </summary>
+        public Base64ConverterForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Form_Load Initialize
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void Base64ConverterForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Base64ConverterForm_DragDrop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Base64ConverterForm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // 日時ベースのフォルダ名を生成
+            var dateTimeFolder = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+            var outputPathDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dateTimeFolder);
+            Directory.CreateDirectory(outputPathDirectory); // フォルダが存在しない場合、作成
+
+            var counter = 0;
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    if (rdoDecode.Checked)
+                    {
+                        var jsonContent = File.ReadAllText(file);
+                        var jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonContent);
+                        var base64String = jsonObject?.inputParam_Base64.ToString();
+
+                        if (string.IsNullOrEmpty(base64String))
+                        {
+                            throw new Exception("inputParam_Base64 が見つかりませんでした");
+                        }
+
+                        var decodedContent = Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+
+                        var outputPath = Path.Combine(outputPathDirectory, Path.GetFileNameWithoutExtension(file) + "_Base64decode.json");
+                        File.WriteAllText(outputPath, decodedContent);
+                        //MessageBox.Show($"デコード成功: {outputPath}");
+                        counter++;
+                    }
+                    else
+                    {
+                        var fileContent = File.ReadAllText(file);
+                        var base64Encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(fileContent));
+
+                        var jsonObject = new { inputParam_Base64 = base64Encoded };
+                        var json = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+
+                        var outputPath = Path.Combine(outputPathDirectory, Path.GetFileNameWithoutExtension(file) + "_Base64encode.json");
+                        File.WriteAllText(outputPath, json);
+                        //MessageBox.Show($"エンコード成功: {outputPath}");
+                        counter++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"エラー: {ex.Message}");
+                }
+            }
+
+            MessageBox.Show($"{counter.ToString()} 件処理しました。", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //System.Diagnostics.Process.Start(outputPathDirectory);
+        }
+
+        /// <summary>
+        /// Base64ConverterForm_DragEnter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Base64ConverterForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+    }
+}
